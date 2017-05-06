42506f679c530f07b6600736fc54c0ba6929348c
Jon Borrelli <borrejj@gmail.com>
2017-05-06 09:53:57 -0400

    add function for cascade model

--- R/topological.R
+++ R/topological.R
@@ -31,3 +31,23 @@ democracy <- function(S, C){
   a[upper.tri(a)] <- rbinom(sum(upper.tri(a)), 1, p)
   return(a)
 }
+
+
+#' Cascade Food Web Model
+#'
+#' @param S Number of species in the community.
+#' @param C Average path length used to get connectance.
+#'
+#' @return The adjacency matrix of a cascade model food web. Species are arranged along a niche axis and may consume any species with a niche value less than their own with a fixed probability of \code{2CS/(S-1)}, creating a triangular matrix.
+#' @export
+#'
+#' @examples
+#' cascade(20, .1)
+cascade <- function(S, C){
+  a <- matrix(0, nrow = S, ncol = S)
+  p <- (2*C*S)/(S-1)
+  for(i in 1:(S-1)){
+    a[i, (i+1):S] <- rbinom(length((i+1):S), 1, p)
+  }
+  return(a)
+}
