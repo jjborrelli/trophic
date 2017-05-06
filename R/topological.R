aaeca5da9719f29ef3ce9f4fbf9bfbbfe3e878f0
Jon Borrelli <borrejj@gmail.com>
2017-05-05 22:43:03 -0400

    add democracy function

--- R/topological.R
+++ R/topological.R
@@ -13,3 +13,21 @@ anarchy <- function(S, C){
   m <- matrix(rbinom(S^2, 1, p), nrow = S, ncol = S)
   return(m)
 }
+
+
+#' Democracy
+#'
+#' @param S Number of species in the community.
+#' @param C Average path length used to get connectance.
+#'
+#' @return An adjacency matrix of a random network where any two species are connected with probability \code{C/S}. this model differs from \code{anarchy} by not allowing any trophic loops.
+#' @export
+#'
+#' @examples
+#' democracy(20, 1.86)
+democracy <- function(S, C){
+  p <- C/S
+  a <- matrix(0, nrow = S, ncol = S)
+  a[upper.tri(a)] <- rbinom(sum(upper.tri(a)), 1, p)
+  return(a)
+}
