2d4de38b69616bef05c1a2965670a5f3e0d7b4c7
Jon Borrelli <borrejj@gmail.com>
2017-05-17 01:08:09 -0400

    change likelihood to loglik

--- R/likelihood.R
+++ R/likelihood.R
@@ -7,11 +7,37 @@
 #'
 #' @examples
 cascade_likelihood<- function(A){
+
   L <- sum(A)
   S <- nrow(A)
   CONN <- L/(S*(S-1))
   p <- (2*CONN*S)/(S-1)
+
   if(sum(A[lower.tri(A)]) == 0){
+
     P.A_p <- (p^L)*(1-p)^((S*(S-1))/2-L)
+    return(log10(P.A_p))
+
+  }else{
+
+    N <- A
+    N[lower.tri(N)] <- 0
+    L <- sum(N)
+    S <- nrow(N)
+    CONN <- L/(S*(S-1))
+    p <- (2*CONN*S)/(S-1)
+    P.A_p <- (p^L)*(1-p)^((S*(S-1))/2-L)
+
+    K <- A
+    K[upper.tri(K)] <- 0
+    L <- sum(K)
+    S <- nrow(K)
+    q <- L/(S*(S-1))
+    P.K_q <- q^L*(1-q)^(S^2-L)
+
+    L.pq_A <- P.A_p * P.K_q
+
   }
+
+  return(log10(L.pq_A))
 }
