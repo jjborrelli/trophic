3de5c4f27774a9f0eea4d84ea2ec90ac42342f4a
jjborrelli <borrejj@gmail.com>
2018-09-14 23:52:36 -0400

    updated plotting funcs

--- R/plotting.R
+++ R/plotting.R
@@ -15,3 +15,26 @@
 avis <- function(a){
   bipartite::visweb(a, type = "none", clear = F)
 }
+
+################################################
+################################################
+##### Visualize network
+
+#' Adjacency Matrix Visualization
+#'
+#' @param a Adjacency matrix
+#'
+#' @return A plot of the adjacency matrix where black cells indicate interactions
+#' @export
+#'
+#' @examples
+#' n <- niche(30, .2)
+#' avis(n)
+netvis_trophic <- function(a){
+  net <- igraph::graph.adjacency(a)
+  tl <- NetIndices::TrophInd(a)
+  lay <- layout.kamada.kawai(net)
+  lay[,2] <- tl$TL
+
+  plot(net, layout = lay)
+}
